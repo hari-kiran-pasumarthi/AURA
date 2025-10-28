e3512dcfc0f9e28a2b3350f0b424fa0e9898859b
hari-kiran-pasumarthi <142865072+hari-kiran-pasumarthi@users.noreply.github.com>
2025-10-28 20:42:48 +0530

    Update flashcards.py

--- backend/routers/flashcards.py
+++ backend/routers/flashcards.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException
-from models.schemas import FlashcardRequest, FlashcardResponse, Flashcard
+from backend.models.schemas import FlashcardRequest, FlashcardResponse, Flashcard
 from utils.save_helper import save_data, save_entry
 from pypdf import PdfReader
 from rake_nltk import Rake
