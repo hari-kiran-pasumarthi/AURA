c4d25ab5e496bc436a268ec3613f694d16b499c6
hari-kiran-pasumarthi <142865072+hari-kiran-pasumarthi@users.noreply.github.com>
2025-10-31 14:18:18 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("✅ Service Worker Registered"))
+      .catch((err) => console.error("⚠️ Service Worker Error:", err));
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
